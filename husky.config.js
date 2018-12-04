00d92a6169b8412be999c37d908d046d3cb2b4a9
lwyj123 <liang.pearce@gmail.com>
2018-12-04 17:30:08 +0800

    feat: add jest unit test

--- husky.config.js
+++
@@ -1,9 +0,0 @@
-const { join } = require("path");
-
-const commitLintConfig = join(__dirname, "commitlint.config.js");
-
-module.exports = {
-  hooks: {
-    "commit-msg": `commitlint -e $GIT_PARAMS --config ${commitLintConfig}`
-  }
-};
